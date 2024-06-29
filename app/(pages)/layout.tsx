abb7c3d231d66e3fa14caf5d20ae3be9ca25244c
Rishabh Singh <rishthakur18@gmail.com>
2024-06-29 19:42:54 +0530

    added dummy products

---
+++ app/(pages)/layout.tsx
@@ -0,0 +1,11 @@
+import Navbar from 'components/layout/navbar';
+import { ReactNode } from 'react';
+
+export default async function Layout({ children }: { children: ReactNode }) {
+  return (
+    <>
+      <Navbar />
+      <main>{children}</main>
+    </>
+  );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,3 @@
-import Navbar from 'components/layout/navbar';
 import { GeistSans } from 'geist/font/sans';
 import { ensureStartsWith } from 'lib/utils';
 import { ReactNode } from 'react';
@@ -20,23 +19,14 @@ export const metadata = {
   robots: {
     follow: true,
     index: true
-  },
-  ...(twitterCreator &&
-    twitterSite && {
-      twitter: {
-        card: 'summary_large_image',
-        creator: twitterCreator,
-        site: twitterSite
-      }
-    })
+  }
 };
 
 export default async function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en" className={GeistSans.variable}>
       <body className="bg-neutral-50 text-black selection:bg-teal-300 dark:bg-neutral-900 dark:text-white dark:selection:bg-pink-500 dark:selection:text-white">
-        <Navbar />
-        <main>{children}</main>
+        {children}
       </body>
     </html>
   );
